e7530ec2c358e6cd6f6f8c260d2e2a4283d6e3a8
doinki <doinki94@gmail.com>
2024-08-03 17:42:20 +0900

    feat: Add createRequestHandler for Elysia

---
+++ src/elysia/index.ts
@@ -0,0 +1,32 @@
+import {
+  type AppLoadContext,
+  createRequestHandler as createRemixRequestHandler,
+  type ServerBuild,
+} from '@remix-run/server-runtime';
+import { type Context } from 'elysia';
+
+export interface GetLoadContextFunction {
+  (c: Context): Promise<AppLoadContext> | AppLoadContext;
+}
+
+export interface RequestHandler {
+  (c: Context): Promise<Response>;
+}
+
+export function createRequestHandler({
+  build,
+  getLoadContext,
+  mode = process.env.NODE_ENV,
+}: {
+  build: ServerBuild | (() => ServerBuild | Promise<ServerBuild>);
+  getLoadContext?: GetLoadContextFunction;
+  mode?: string;
+}): RequestHandler {
+  let handleRequest = createRemixRequestHandler(build, mode);
+
+  if (getLoadContext) {
+    return async (c) => handleRequest(c.request, await getLoadContext(c));
+  }
+
+  return (c) => handleRequest(c.request);
+}
